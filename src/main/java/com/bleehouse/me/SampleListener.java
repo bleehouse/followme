effedcb3669c34cb7231d66eee0b889908004947
Byungho Lee <blee03291@gmail.com>
2020-02-20 13:30:58 +0900

    ApplicationStartedEvent Sample

--- src/main/java/com/bleehouse/me/SampleListener.java
+++ src/main/java/com/bleehouse/me/SampleListener.java
@@ -1,15 +1,15 @@
 package com.bleehouse.me;
 
-import org.springframework.boot.context.event.ApplicationStartingEvent;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
-@Component
-public class SampleListener implements ApplicationListener<ApplicationStartingEvent> {
+//@Component
+public class SampleListener implements ApplicationListener<ApplicationStartedEvent> {
     @Override
-    public void onApplicationEvent(ApplicationStartingEvent applicationStartingEvent) {
+    public void onApplicationEvent(ApplicationStartedEvent ApplicationStartedEvent) {
         System.out.println("=================================================");
-        System.out.println("Application is Starting");
+        System.out.println("Application is Started");
         System.out.println("=================================================");
     }
 }
